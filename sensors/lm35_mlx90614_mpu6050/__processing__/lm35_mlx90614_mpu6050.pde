c83f7431da970c7da0729bd8ce58208d257df29b
Marcelo Cardoso <marcelovicentegc@gmail.com>
2019-05-16 16:35:49 -0300

    Added lm35 + mlx90614 + mpu6050 code and datalog

---
+++ sensors/lm35_mlx90614_mpu6050/__processing__/lm35_mlx90614_mpu6050.pde
@@ -0,0 +1,72 @@
+import processing.serial.*;
+
+Serial dataLogger;
+
+Table dataTable;
+
+String logPath = "../../__datalog__/";
+String fileName;
+String port = "COM7";
+
+boolean test = true;
+int numReadings;
+int readingCounter;
+
+void setup () {
+    dataLogger = new Serial(this, port, 9600);
+    dataTable = new Table();
+
+    if (test == true) {
+        numReadings = 20;
+        readingCounter = 0;
+    }
+
+    dataTable.addColumn("id");
+    dataTable.addColumn("date");
+    dataTable.addColumn("time");
+    dataTable.addColumn("LM35Temperature");
+    dataTable.addColumn("MLX90614AmbientTemperature");
+    dataTable.addColumn("MLX90614ObjectTemperature");
+}
+
+void serialEvent(Serial dataLogger) {
+    try {
+        String val = dataLogger.readStringUntil('\n');
+        
+        if (val != null) {
+            val = trim(val);
+            float sensorVals[] = float(split(val, ','));
+            println(sensorVals);
+            
+            TableRow newRow = dataTable.addRow(); 
+            newRow.setInt("id", dataTable.lastRowIndex());
+            newRow.setString("date", year() + "-" + month() + "-" + day());
+            newRow.setString("time", hour() + ":" + minute() + ":" + second());
+            newRow.setFloat("LM35Temperature", sensorVals[0]);
+            newRow.setFloat("MLX90614AmbientTemperature", sensorVals[1]);
+            newRow.setFloat("MLX90614ObjectTemperature", sensorVals[2]);
+            newRow.setFloat("AcX", sensorVals[3]);
+            newRow.setFloat("AcY", sensorVals[4]);
+            newRow.setFloat("AcZ", sensorVals[5]);
+            newRow.setFloat("GyX", sensorVals[6]);
+            newRow.setFloat("GyY", sensorVals[7]);
+            newRow.setFloat("GyY", sensorVals[8]);
+
+            if (test == true) {
+                readingCounter++;
+
+                if (readingCounter % numReadings == 0) {
+                    fileName = str(year()) + "-" + str(month()) + "-" + str(day()) + "-" + str(dataTable.lastRowIndex()) + ".csv";
+                    println(fileName);
+                    saveTable(dataTable, logPath + fileName);
+                }
+            }
+        }
+    } catch(RuntimeException error) {
+    error.printStackTrace();
+    }
+}
+
+void draw()
+{
+}
---
+++ sensors/lm35_mlx90614_mpu6050/lm35_mlx90614_mpu6050.ino
@@ -0,0 +1,155 @@
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+
+const int sensor = A0;
+float rawSensorData;
+int testLoop = 20;
+bool test = false;
+
+// lm35
+bool getKelvin = false;
+bool getFahrenheit = false;
+
+// mpu6050
+int AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+const int MPU = 0x68;
+bool saveToTxt = false;
+bool saveToCsv = true;
+
+void setup()
+{
+    pinMode(sensor, INPUT);
+    Serial.begin(9600);
+    mlx.begin();
+    Wire.begin();
+    Wire.beginTransmission();
+    Wire.write(0x6B);
+}
+
+void getTemperatures()
+{
+    rawSensorData = analogRead(sensor);
+    float celsius = (rawSensorData * 500) / 1024;
+    float fahrenheit = (celsius * 1.8) + 32;
+
+    if (saveToTxt == true)
+    {
+        // Temperatura arrefecimento
+        Serial.print("LM35 temp.: ");
+        Serial.print(celsius);
+        Serial.print(" C");
+        Serial.println();
+
+        // Infravermelho (disco de freio)
+        Serial.print("MLX90614 ambient temp.: ");
+        Serial.print(mlx.readAmbientTempC());
+        Serial.print(" C");
+        Serial.println();
+        Serial.print("MLX90614 object temp.: ");
+        Serial.print(mlx.readObjectTempC());
+        Serial.print(" C");
+        Serial.println();
+
+        // Acelerômetro
+        Serial.print("AcX = ");
+        Serial.print(AcX);
+        Serial.print(" | AcY = ");
+        Serial.print(AcY);
+        Serial.print(" | AcZ = ");
+        Serial.print(AcZ);
+        Serial.print(" | Tmp = ");
+        Serial.print(Tmp / 340.00 + 36.53);
+        Serial.print(" | GyX = ");
+        Serial.print(GyX);
+        Serial.print(" | GyY = ");
+        Serial.print(GyY);
+        Serial.print(" | GyZ = ");
+        Serial.println(GyZ);
+        delay(2000);
+
+        if (getKelvin == true)
+        {
+            float kelvin = celsius + 273.15;
+            float ambientKelvin = mlx.readAmbientTempC() + 273.15;
+            float objectKelvin = mlx.readObjectTempC() + 273.15;
+            Serial.print("LM35 temperature ");
+            Serial.print(kelvin);
+            Serial.print(" K");
+            Serial.println();
+            Serial.print("MLX90614 ambient temperature: ");
+            Serial.print(ambientKelvin);
+            Serial.print(" K");
+            Serial.println();
+            Serial.print("MLX90614 object temperature: ");
+            Serial.print(objectKelvin);
+            Serial.print(" K");
+            Serial.println();
+        }
+
+        if (getFahrenheit == true)
+        {
+
+            Serial.print("LM35 temperature: ");
+            Serial.print(fahrenheit);
+            Serial.print(" F");
+            Serial.println();
+            Serial.print("MLX90614 ambient temperature: ");
+            Serial.print(mlx.readAmbientTempF());
+            Serial.print(" F");
+            Serial.println();
+            Serial.print("MLX90614 object temperature: ");
+            Serial.print(mlx.readObjectTempF());
+            Serial.print(" F");
+            Serial.println();
+        }
+    }
+
+    if (saveToCsv == true)
+    {
+        Serial.print(mlx.readAmbientTempC());
+        Serial.print(",");
+        Serial.print(mlx.readObjectTempC());
+        Serial.print(",");
+        Serial.print(celsius);
+        Serial.print(",");
+        Serial.print(AcX);
+        Serial.print(",");
+        Serial.print(AcY);
+        Serial.print(",");
+        Serial.print(AcZ);
+        Serial.print(",");
+        Serial.print(GyX);
+        Serial.print(",");
+        Serial.print(GyY);
+        Serial.print(",");
+        Serial.print(GyZ);
+        Serial.println();
+        delay(2000);
+    }
+}
+
+void loop()
+{
+    if (test == true)
+    {
+        for (int a = 0; a < testLoop; a++)
+        {
+            getTemperatures();
+        }
+        Serial.print("Test suit completed.");
+        Serial.println();
+
+        // Since Arduino has no O.S., exit() is useless. There is no correct way to stop an Arduino program
+        // since it is supposed to loop forever. For output purposes, calling while(1) {} traps the program
+        // into a outputless loop, "stopping" it.
+        while (1)
+        {
+        }
+    }
+    else
+    {
+        getTemperatures();
+    }
+}
